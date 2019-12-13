0930259949daec9e4cd8200b407f3fc337db03b7
Lia Yuliana <liayuliana291@gmail.com>
2019-12-13 14:49:01 +0700

    [GREEN] Test FAQ template

--- frequentlyasked/views.py
+++ frequentlyasked/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def frequentlyasked(request):
-    return render(request, 'contact/index.html')
+    return render(request, 'frequentlyasked/index.html')
 
